bb8c856ca07fe370dae68c27f4effc252f16bb27
AlmiraTolstova <almiratolstova@gmail.com>
2021-12-01 00:13:35 +0300

    Доработка css OpenPosts, UserProfilie

--- src/forms/OpenPost/OpenPost.js
+++ src/forms/OpenPost/OpenPost.js
@@ -6,53 +6,58 @@ import commentReducer from "../../reducers/commentReducer";
 import Post from "../../components/Post/Post";
 
 
-const OpenPost =({title,firstName,lastName, dataPost,imgUrl, textPost})=>{
+const OpenPost = ({title, firstName, lastName, dataPost, imgUrl, textPost}) => {
 
-    const [openPostActive,setOpenPost]=useState(false);
-    const statePR = useSelector((state=>state.openPostReducer));
+    const [openPostActive, setOpenPost] = useState(false);
+    const statePR = useSelector((state => state.openPostReducer));
     const dispatch = useDispatch();
-    const stateCR = useSelector(state=>state.commentReducer);
+    const stateCR = useSelector(state => state.commentReducer);
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log(stateCR);
         console.log(statePR.showPostComments);
         console.log("хук обновления содержимого");
-    },[stateCR]);
+    }, [stateCR]);
 
 
+    return (
+        <div className={statePR.showPostComments ? "modal active" : "modal"} onClick={(e) => e.stopPropagation()}>
+            <div className="modal__open-post">
 
-   return(
-       <div className={statePR.showPostComments ? "modal active":"modal"} onClick={(e)=>e.stopPropagation()}>
-           <div className="modal__open-post" >
-           <div>
-               {`${title}  ${firstName} ${lastName} ${dataPost}`}
-           </div>
-           <div>
-               <img className="modal__img" src={imgUrl}/>
-           </div>
-           <div>
-               {textPost}
-           </div>
-               <div>
-                   <button onClick={()=>{dispatch({type:HIDE_POST_WITH_COMMENTS, payload: false});
-                       console.log("показывать окно? ",statePR.showPostComments)}}>
-                       Закрыть окно
-                   </button>
-               </div>
-               {stateCR.listComments.length !=0
-                   ? stateCR.listComments.map((elem, index) => (
-                       <div >
-                           {elem.message}
-                           {elem.publishDate}
-                           {elem.owner.title}
-                           {elem.owner.firstName}
-                           {elem.owner.lastName}
-                           <img src={elem.owner.picture}/>
-                       </div>
-                   )):"Пока никто не оставил комментариев..."}
-           </div>
-       </div>
-   )
+                    <div className="modal__btn">
+                        <button onClick={() => {
+                            dispatch({type: HIDE_POST_WITH_COMMENTS, payload: false});
+                            console.log("показывать окно? ", statePR.showPostComments)
+                        }}>
+                            Закрыть окно
+                        </button>
+                    </div>
+                <div className="modal__container">
+                    <div className="modal__title">
+                        {`${title}  ${firstName} ${lastName} ${dataPost}`}
+                    </div>
+
+                        <img className="modal__img" src={imgUrl}/>
+
+                    <div className="modal__text">
+                        {textPost}
+                    </div>
+
+                    {stateCR.listComments.length != 0
+                        ? stateCR.listComments.map((elem, index) => (
+                            <div>
+                                {elem.message}
+                                {elem.publishDate}
+                                {elem.owner.title}
+                                {elem.owner.firstName}
+                                {elem.owner.lastName}
+                                <img src={elem.owner.picture}/>
+                            </div>
+                        )) : "Пока никто не оставил комментариев..."}
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export  default OpenPost
+export default OpenPost
--- src/forms/UserProfile/UserProfile.js
+++ src/forms/UserProfile/UserProfile.js
@@ -1,17 +1,16 @@
 import React, {useEffect, useState} from "react";
-import {Form} from "antd";
+import {Button, Form} from "antd";
 import 'antd/dist/antd.css';
 import {Link} from "react-router-dom";
 import {EMPTY_STRING} from "../../constants/api/common";
 import {useDispatch, useSelector} from "react-redux";
 import {updateUser} from "../../api/dumMyApi";
 import loginReducer from "../../reducers/loginReducer";
+import "./UserProfilie.css"
 
 
-
-
-const UserProfile=()=>{
-    const [img, setImg]=useState(EMPTY_STRING);
+const UserProfile = () => {
+    const [img, setImg] = useState(EMPTY_STRING);
     const [name, setName] = useState(EMPTY_STRING);
     const [gender, setGender] = useState(EMPTY_STRING);
     const [dateOfBirth, setDateOfBirth] = useState(EMPTY_STRING)
@@ -19,28 +18,26 @@ const UserProfile=()=>{
     const [firstName, setFirstName] = useState(EMPTY_STRING);
     const [SecondName, setSecondName] = useState(EMPTY_STRING);
 
-    const statePAR = useSelector((state=>state.personalAreaReducer));
-    
+    const statePAR = useSelector((state => state.personalAreaReducer));
+
     const dispatch = useDispatch();
 
-    useEffect(()=>{
+    useEffect(() => {
         setImg(statePAR.picture);
-        setName(statePAR.firstName+" "+statePAR.lastName);
-        setGender(statePAR.gender=="male"?"Мужской":"Женский");
+        setName(statePAR.firstName + " " + statePAR.lastName);
+        setGender(statePAR.gender == "male" ? "Мужской" : "Женский");
         setDateOfBirth(statePAR.dateOfBirth);
         setPhone(statePAR.phone);
-    },[])
+    }, [])
 
-    const onSendNewInfoAboutUser=()=>{
+    const onSendNewInfoAboutUser = () => {
 
         const namming = name;
         const namePos = namming.indexOf(' ');
-        if(namePos>0){
+        if (namePos > 0) {
             setFirstName(namming.slice(0, namePos));
             setSecondName(namming.slice(namePos, namming.length));
-        }
-        else
-        {
+        } else {
             setFirstName(namming);
             setSecondName('notLastName');
         }
@@ -48,32 +45,35 @@ const UserProfile=()=>{
         dispatch(updateUser(statePAR.id, firstName, SecondName, dateOfBirth, phone));
     }
 
-    return(
-        <Form>
-            <img src={img}/>
-            <button>Обновить фотографию</button>
-            <button>Удалить фотографию</button>
-            <Form.Item>
-            <span>Имя: </span>
-                <input value={name} onChange={(e)=>setName(e.target.value)}/>
-            </Form.Item>
-            <Form.Item>
-                <span>Пол: </span>
-                <input value={gender} />
-            </Form.Item>
-            <Form.Item>
-                <span>Дата рождения: </span>
-                <input value={dateOfBirth} onChange={(e)=>setDateOfBirth(e.target.value)}/>
-            </Form.Item>
-            <Form.Item>
-                <span>Телефон </span>
-                <input value={phone} onChange={(e)=>setPhone(e.target.value)}/>
-            </Form.Item>
-            <Link>
-                <button onClick={onSendNewInfoAboutUser}>Сохранить</button>
-            </Link>
-
-        </Form>
+    return (
+        <div className="user-profile">
+            <div className="user-profile__container">
+                <img className="user-profile__img" src={img}/>
+                <div className="user-profile__bts">
+                <button>Обновить фотографию</button>
+                <button>Удалить фотографию</button>
+                </div>
+                <Form.Item>
+                    <span>Имя: </span>
+                    <input value={name} onChange={(e) => setName(e.target.value)}/>
+                </Form.Item>
+                <Form.Item>
+                    <span>Пол: </span>
+                    <input value={gender}/>
+                </Form.Item>
+                <Form.Item>
+                    <span>Дата рождения: </span>
+                    <input value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)}/>
+                </Form.Item>
+                <Form.Item>
+                    <span>Телефон </span>
+                    <input value={phone} onChange={(e) => setPhone(e.target.value)}/>
+                </Form.Item>
+                <Link>
+                    <Button type="primary" onClick={onSendNewInfoAboutUser}>Сохранить</Button>
+                </Link>
+            </div>
+        </div>
 
     )
 };
